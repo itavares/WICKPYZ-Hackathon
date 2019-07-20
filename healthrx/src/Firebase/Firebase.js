0a55c136ce1609f7131cf4c35d8d8a1e41f85635
Ighor <f.tavares@wustl.edu>
2019-07-20 11:43:29 -0500

    firebase adde

--- healthrx/src/Firebase/Firebase.js
+++ healthrx/src/Firebase/Firebase.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import app from'firebase/app';
 
-const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
-);
 
-export default App;
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
