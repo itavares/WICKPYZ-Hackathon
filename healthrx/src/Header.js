911c7556a3db44a6b1889e6e0c6f78b6dd9483f1
Pyi Them Kyaw <pyithemkyaw@codefi-0701.resnet.semo.edu>
2019-07-20 10:16:49 -0500

    Created Home page

--- healthrx/src/Header.js
+++ healthrx/src/Header.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 
 class Header extends Component {
 
+    render() {
+        return (
+            <h1> Header </h1>
+        );
+    }
 }
 
 export default Header;
---
+++ healthrx/src/Home/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './Home.css'
+import { Row, Col, Container } from 'react-bootstrap'
+
+class Home extends Component {
+    render() {
+        return (
+            <Container>
+                <Row className="h-100">
+                    <Col id="intro" className="my-auto">
+                        <span className="align-middle text-center">Welcome To Health Rx.</span>
+                    </Col> 
+                    <Col>
+                        <div id="options" className="align-middle text-center">
+                            <h1>Are you a: </h1>
+                            <button type="button" onclick="window.location.href='doctor.html'" class="btn btn-info">Doctor</button> 
+                            <h3> or </h3>
+                            <button type="button" onclick="window.location.href='patient.html'" class="btn btn-info">Patient</button>
+                            <p>New? <a href="createaccount.html"> Create a account </a> </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default Home;
