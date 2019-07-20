f764f45f850dc2607ea6032f350410b548beab81
Ighor <f.tavares@wustl.edu>
2019-07-20 17:08:31 -0500

    logout function

--- healthrx/src/Home/Home.js
+++ healthrx/src/Home/Home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import './Home.css'
 import { Row, Col, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+
  
 import * as ROUTES  from '../constants/routes';
 
 class Home extends Component {
     render() {
         return (
-            <Container>
+            <Container> 
                 <Row className="h-100">
                     <Col id="intro" className="my-auto">
                         <span className="align-middle text-center">Welcome To Health Rx.</span>
@@ -19,12 +20,13 @@ class Home extends Component {
 
                             <button type="button" onclick="window.location.href='doctor.html'" class="btn btn-info home-page-btn"> <Link to={ROUTES.LOGIN_DOCTOR}> Doctor</Link></button> 
                             <h3> or </h3>
-                            <button type="button" onclick="window.location.href='patient.html'" class="btn btn-info home-page-btn"><Link to={ROUTES.LOGIN_DOCTOR}> Patient</Link></button>
+                            <button type="button" onclick="window.location.href='patient.html'" class="btn btn-info home-page-btn"><Link to={ROUTES.LOGIN_PATIENT}> Patient</Link></button>
 
 
 
                             <p>New?<Link to={ROUTES.ACCOUNT}> Create a account </Link> </p>
                         </div>
+
                     </Col>
                 </Row>
             </Container>
--- healthrx/src/Login/PatientLogin.js
+++ healthrx/src/Login/PatientLogin.js
@@ -2,32 +2,101 @@ import React, { Component } from 'react'
 import './Login.css'
 import { Form } from 'react-bootstrap';
 
+
+import {  withFirebase } from '../Firebase';
+import * as ROUTES from '../constants/routes'
+import { withRouter } from 'react-router-dom';
+import {compose} from 'recompose';
+
+
+
+const PatientLoginPage = () =>(
+    <div>
+        <h2> Patient Login </h2>
+        <PatientLoginForm/>
+    </div>
+
+
+)
+
+
+const INITIAL_STATE ={
+    email: '',
+    password: '',
+    error: null,
+};
+
+
+
 class PatientLogin extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = { ...INITIAL_STATE };
+      }
+      onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+      };
+
+    onSubmit = event => {
+        const { email, password } = this.state;
+
+        this.props.firebase
+          .doSignInWithEmailAndPassword(email, password)
+          .then(() => {
+            this.setState({ ...INITIAL_STATE });
+            this.props.history.push(ROUTES.HOME); // REDIRECTS TO USERS HOME 
+          })
+          .catch(error => {
+            this.setState({ error });
+          });
+    
+        event.preventDefault();
+
+    };
+
     render() {
+        const { email, password, error } = this.state;
+        const isInvalid = password === '' || email === '' ;
+
+
         return (
             <div id="login">
-                <h2> Patient Login </h2>
+                {/* <h2> Patient Login </h2> */}
                 <div id="facelogo"> 
                     <i class="far fa-user"></i>
                 </div>
                 <h1>Sign in</h1> 
-                <form>
-                    <Form.Group controlId="formBasicEmail" id="input">
+                <form onSubmit={this.onSubmit}>
+                    <Form.Group  id="input"  name="email">
                         <i class="far fa-user"></i>
                         {/* <input type="text" class="form-control" id="user" name="user" placeholder="Enter username"> </input> */}
-                        <Form.Control type="text" placeholder="Enter username" />
+                        <Form.Control onChange={this.onChange}  value={email} type="text" placeholder="Enter username"  name="email" />
                     </Form.Group>
+
+
                     <div class="form-group" id="input">
                         <i class="fas fa-key"></i>
                         {/* <input type="password" class="form-control" id="password" name="password" placeholder="Enter password"> </input> */}
-                        <Form.Control type="password" name="password" placeholder="Enter password" />
+                        <Form.Control type="password" name="password" onChange={this.onChange} value={password} placeholder="Enter password" />
                         {/* <i id="showpass" class="far fa-eye"></i>   */}
                     </div>
-                    <button type="submit" class="btn btn" id="loginb">Login</button>
+                    <button  type="submit" class="btn btn" id="loginb">Login</button>
+
+                    {error && <p>{error.message}</p>}
                 </form>
             </div>
         );
     }
 }
 
-export default PatientLogin;
+const PatientLoginForm = compose(
+    withRouter,
+    withFirebase,
+)(PatientLogin);
+
+
+export default PatientLoginPage;
+
+export {PatientLoginForm};
