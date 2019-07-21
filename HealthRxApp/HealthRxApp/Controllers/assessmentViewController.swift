ebef4572d97bec7a7996ed7e957130537d1ed810
willparkinson7 <willparkinson7@gmail.com>
2019-07-21 03:43:02 -0500

    Flip yes and no and incremented cards

---
+++ HealthRxApp/HealthRxApp/Controllers/assessmentViewController.swift
@@ -0,0 +1,96 @@
+//
+//  assessmentViewController.swift
+//  HealthRxApp
+//
+//  Created by William Parkinson on 7/20/19.
+//  Copyright © 2019 Zayid Oyelami. All rights reserved.
+//
+
+import UIKit
+
+class assessmentViewController: UIViewController {
+    let questions = ["1","2","3"]
+    var currInd = 0
+    let orgPosX = 0
+    let orgPosY = 0
+    
+    @IBOutlet weak var question: UILabel!
+    
+    @IBOutlet weak var AnswerImg: UIImageView!
+
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var cardBoard: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        question.text = questions[currInd]
+        print(card.center)
+//        print("HERE")
+//        print(orgPosX!)
+//        print(orgPosY!)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer){
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+       if xFromCenter > 0 {
+            AnswerImg.image = UIImage(named:"Yes")
+            AnswerImg.tintColor = UIColor.purple
+        } else{
+            AnswerImg.image = UIImage(named:"No")
+            AnswerImg.tintColor = UIColor.blue
+        }
+            AnswerImg.alpha = abs(xFromCenter) / view.center.x
+        
+        if sender.state == UIGestureRecognizer.State.ended{
+            if card.center.x < 60 {
+                //Move off to the left side
+                // No
+                UIView.animate(withDuration: 0.3, animations:{ card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                    card.alpha = 0
+                    
+                })
+                
+                self.currInd += 1
+                self.question.text = self.questions[self.currInd]
+                UIView.animate(withDuration: 0.85, animations:{ self.card.center = CGPoint(x: 207, y: 445.5)
+                    card.alpha = 1
+                    self.AnswerImg.alpha = 0
+                })
+                
+            } else if card.center.x > (view.frame.width - 60){
+                //Move off to the right side
+                // Yes
+                UIView.animate(withDuration: 0.85, animations:{ card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                    card.alpha = 0
+                    
+                })
+                self.currInd += 1
+                self.question.text = self.questions[self.currInd]
+                UIView.animate(withDuration: 0.85, animations:{ self.card.center = CGPoint(x: 207, y: 445.5)
+                    card.alpha = 1
+                    self.AnswerImg.alpha = 0
+                })
+            }
+            
+        }
+    }
+    @IBAction func restart(_ sender: UIButton) {
+        restartCard()
+    }
+    
+    func restartCard(){
+        UIView.animate(withDuration: 0.2, animations: {self.card.center = self.view.center
+            self.AnswerImg.alpha = 0
+            self.currInd -= 1
+            self.question.text = self.questions[self.currInd]
+            self.card.alpha = 1
+        })
+    }
+    
+}
